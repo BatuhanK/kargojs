4f3d5f852ccd9541a30461068ebc889a98d30cf3
Yagiz Nizipli <yagiznizipli@me.com>
2015-09-01 18:02:14 +0300

    Update sample file after index change

--- sample.js
+++ sample.js
@@ -1,31 +1,21 @@
-var kargojs = require('./index')();
+var kargojs = require('./index');
+
 
 kargojs.Yurtici.query("107683143283",function(err,data){
-    if(err){
-    console.log(err);
-    } else{
-    console.log(data);
-    }
-})
-kargojs.Yurtici.getMovements("107683143283",function(err,data){
-    if(err){
-    console.log(err);
-    } else{
-    console.log(data);
-    }
-})
-kargojs.Aras.query("5560228801279",function(err,data){
-    if(err){
-        console.log(err);
-    } else{
-        console.log(data);
-    }
+    console.log(err ? err : data);
 })
 
+
+kargojs.Yurtici.getMovements("107683143283",function(err,data){
+    console.log(err ? err : data);
+})
+
+
+kargojs.Aras.query("5560228801279",function(err,data){
+    console.log(err ? err : data);
+})
+
+
 kargojs.Aras.getMovements("5560228801279",function(err,data){
-    if(err){
-        console.log(err);
-    } else{
-        console.log(data);
-    }
+    console.log(err ? err : data);
 })
