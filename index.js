5c63edf2192ad56bcba67545fad713ed28f83844
Yagiz Nizipli <yagiznizipli@me.com>
2015-09-01 18:01:46 +0300

    Remove unused settings from index

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-var Yurtici = require('./firmalar/Yurtici');
-var Aras = require('./firmalar/Aras');
-
-function KargoJS(settings){
-    this.Yurtici = Yurtici;
-    this.Aras = Aras;
-}
-
-module.exports = function(settings){
-    return new KargoJS(settings);
-}
+module.exports = require('./companies')
